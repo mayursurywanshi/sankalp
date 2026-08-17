54514d0e6a4d111c4032411164cd7b6fc691d257
mayursurywanshi <surywanshi.mr@gmail.com>
2026-08-17 12:51:09 +0530

    Home API

---
+++ backend/src/constants/home.constants.ts
@@ -0,0 +1,4 @@
+export const HOME_CONTENT = {
+  title: "Welcome to Sankalp Sankalp physiotherapy and child development clinic",
+  description: "Compassionate physiotherapy and developmental care to help every child move, grow, and thrive",
+} as const;
